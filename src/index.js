30d02934037fb36d4ad5bb314b327e5d25d0a3c5
casannorfaisa-gif <casannorfaisa@gmail.com>
2026-02-28 02:44:16 +0800

    feat: add express application entry point
    Full Name: Norfaisa Casan
    Umindanao Email: n.casan.535258@umindanao.edu.ph

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import { config } from './config/index.js';
-
-console.log(`Server running on port ${config.port}`);
-console.log(`API URL: ${config.pokeapi.baseUrl}`);
-console.log(`Items per page: ${config.pagination.defaultLimit}`);
